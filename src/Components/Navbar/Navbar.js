640eb9cd6ec76d8e22655b29a67bb795e2983ce3
Ivy Nasreen <ivynasrn@gmail.com>
2021-06-20 16:33:07 +0600

    done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREcwP-iPnYgUQ6QIZMUap_QMpOe8fikDvaYw&usqp=CAU" alt="" width="120" /><strong className = "text-white ms-3">Kids Charity</strong></a>
+                <a class="navbar-brand" href="https://www.charity.com"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREcwP-iPnYgUQ6QIZMUap_QMpOe8fikDvaYw&usqp=CAU" alt="" width="120" /><strong className = "text-white ms-3">Kids Charity</strong></a>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
